cb9c849f06a659413de756debc1105e730850ab2
Roberto Zepeda Hipolito <robertozh29hotmail.com>
2022-11-16 17:09:44 -0600

    R18: Font awesome dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home() {
   return (
@@ -10,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Tienda en linea Joyeria Kable</h1>
+      <FontAwesomeIcon icon={faEnvelope} style={{width: '40px'}}/>
     </div>
   )
 }
